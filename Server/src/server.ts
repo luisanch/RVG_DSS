6b0712be4c58b5bcbe7862d751a1e120f8541d28
luisanch <luissanchezmtn@gmail.com>
2023-07-27 13:48:39 +0200

    Server: Unity utils removed
    Unity related code deleted from server

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -19,17 +19,17 @@ export const createServer = (config :Options): express.Application => {
   app.use(bodyParser.json());
   app.get('/config', (req, res) => res.json({ useWebSocket: config.websocket, startupMode: config.mode, logging: config.logging }));
   app.use('/signaling', signaling);
-  app.use(express.static(path.join(__dirname, '../public')));
-  app.get('/', (req, res) => {
-    const indexPagePath: string = path.join(__dirname, '../public/index.html');
-    fs.access(indexPagePath, (err) => {
-      if (err) {
-        log(LogLevel.warn, `Can't find file ' ${indexPagePath}`);
-        res.status(404).send(`Can't find file ${indexPagePath}`);
-      } else {
-        res.sendFile(indexPagePath);
-      }
-    });
-  });
+  // app.use(express.static(path.join(__dirname, '../public')));
+// app.get('/', (req, res) => {
+//   const indexPagePath: string = path.join(__dirname, '../public/index.html');
+//   fs.access(indexPagePath, (err) => {
+//     if (err) {
+//       log(LogLevel.warn, `Can't find file ' ${indexPagePath}`);
+//       res.status(404).send(`Can't find file ${indexPagePath}`);
+//     } else {
+//       res.sendFile(indexPagePath);
+//     }
+    // });
+  // });
   return app;
 };
